9aa3cbcbbe9f3e8a2efc4a2ca42d5c325db8d99b
shripadashtekar <shripadashtekar7@gmail.com>
2015-02-12 15:31:57 +0530

    More stable version

--- phaseone.cpp
+++ phaseone.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include<string.h>
 #include<string>
 #include<iostream>
 #include<stdlib.h>
@@ -9,21 +8,18 @@ ifstream in;
 ofstream out;
 string line;
 int cnt=0,i=0,j=0;
+
 class memory {
-    
-    
-private:
+public:
     char m[100][4];
     
 public:
     void resetmem()
     {
-     //    cout<<"I am here 5";
-        //for( i=0;i<100;i++)
-          //  for(j=0;j<4;j++)
-            //    m[i][j]=0;
+     
         memset(m,'0',sizeof(char)*100*4);
     }
+    
     string getmem(int position)
     {
         string temp="";
@@ -38,11 +34,11 @@ public:
         for(int i=0;i<4;i++)
             m[position][i]=s[i];
     }
+    
     void StoreCardInMemory(string card, int location)
     {
         cnt=0;
         
-         //cout<<"I am here 6";
         for(i=0;i<card.length();i++)
         {
             for(j=0;j<4;j++)
@@ -51,8 +47,6 @@ public:
                 break;
 
                 m[location][j]=card[cnt];
-                
-                //cout<<i<<j<<location<<cnt<<" "<<m[i][j]<<endl;
                 cnt++;
                 //out<<m[location][j];
             }
@@ -60,28 +54,29 @@ public:
            
         }
         i=j=cnt=0;
-        //out<<endl;
+       // out<<endl;
         
     }
     
     void putdatafrommemory(int location)
     {
-        string temp="",temp1="";
-        //out<<"Putting data";
+        //out<<"executed"<<location<<endl;
+        string temp="";
         for(i=0;i<10;i++)
         {
             temp.clear();
-            temp1.clear();
-          temp=getmem(location);
+            temp=getmem(location);
             for(j=0;j<4;j++)
             {
-                if(temp[j]=='\0'||temp[j]=='0')
+                if(temp[j]=='\0')
                     break;
+                else if(temp[j]=='0')
+                    out<<" ";
                 else
-                    temp1=temp1+temp[j];
+                    out<<temp[j];
             
             }
-            out<<temp1;
+            
             location++;
             
         }
@@ -93,8 +88,8 @@ public:
 class cpu
 {
 private:
-    char IR[4],IC[2],R[4],C='F',terminated='F';             //Declaration of variables IC, R, IR, C;
-    int returnedIC,returnedconvIR,returnedconvR,ctemp,SI=0;
+    char IR[4],IC[2],R[4],C,terminated;             //Declaration of variables IC, R, IR, C;
+    int returnedIC,returnedconvIR,returnedconvR,ctemp,SI;
     string returnedIR,returnedR,temp,operand,opcode,temp1;
     
 public:
@@ -103,13 +98,10 @@ public:
     void setIC(int val)     //seperate digits and store in IC by converting it into character;
     
         {
-            // cout<<"I am here 7";
             int y=val % 10;
             IC[1]=y+'0';
             int x=val/10;
             IC[0]=x+'0';
-            //cout<<mem.m[0][0]<<mem.m[0][1]<<mem.m[0][2]<<mem.m[0][3]<<endl;
-           
            
         }
     
@@ -119,45 +111,20 @@ public:
         return val;
     }
     
-    int getIC()
+    int getIC() //extract the characters, convert to a number and return
     {
-         //cout<<"I am here 8";
-        //extract the characters, convert to a number and return
         int val=((int)IC[0]-'0')*10+((int)IC[1]-'0');
-        
-       // cout<<(int)IC[0]<<(int)IC[1]<<endl;
         return val;
         
     }
     
-    void setIR(int position)
+    void setIR(int position) // get from mem and store in IR
     {
         string temp="";
         temp=mem.getmem(position);
         for(int i=0;i<4;i++)
             IR[i]=temp[i];
         
-        
-        
-         //cout<<"I am here 9";
-        // get from mem and store in IR
-        
-        //cout<<position<<endl;
-       /* j=0;
-        IR[0]=getmem(position);
-        //cout<<IR[0];
-            j++;
-        IR[1]=mem.m[position][j];
-        //cout<<IR[1];
-            j++;
-        IR[2]=mem.m[position][j];
-        //cout<<IR[2];
-            j++;
-        IR[3]=mem.m[position][j];*/
-        //cout<<IR[3];
-       // cout<<mem.m[0][0]<<mem.m[0][1]<<mem.m[0][2]<<mem.m[0][3]<<endl<<endl;
-        
-        //cout<<IR[0]<<IR[1]<<IR[2]<<IR[3]<<endl;
     }
     
     void incIC()
@@ -189,14 +156,13 @@ public:
         
     }
    
-    void setR(string temp)
+    void setR(string temp)        // load into register from memory;
+
     {
         R[0]=temp[0];
         R[1]=temp[1];
         R[2]=temp[2];
         R[3]=temp[3];
-        
-        // load into register from memory;
     }
     
     string getR()
@@ -211,46 +177,44 @@ public:
     
     void  StartExecution()
     {
-        // cout<<"I am here 107"<<endl;
         setIC(0);
         while (terminated!='T')
         {
-            //fetch instruction from ic memory location
-            //store instruction in IR
             returnedIC=getIC();
-            //cout<<returnedIC<<endl;
             setIR(returnedIC);
-            //store M[IC] into IR;
             incIC();
             returnedIR=getIR();
-        opcode=returnedIR.substr(0,2);
-           // cout<<opcode<<endl;
-        operand=returnedIR.substr(2,2);
-        ctemp=stringtoint(operand);
-            //cout<<ctemp<<endl;
+            opcode=returnedIR.substr(0,2);
+            operand=returnedIR.substr(2,2);
+            ctemp=stringtoint(operand);
             if(opcode.find("GD")!=-1)
             {
+             //   out<<"getdata called"<<endl;
+
                 SI=1;
+                 mos();
                 
             }
             if(opcode.find("PD")!=-1)
             {
+               // out<<"putdata called"<<endl;
                 SI=2;
+                 mos();
                 
             }
             if(opcode.find("LR")!=-1)
             {
-                //cout<<"here 11";
+               // out<<"LR called"<<ctemp<<endl;
+
                 temp=mem.getmem(ctemp);
                 setR(temp);
-                temp=getR();
-               // cout<<temp<<endl;
             
             }
             
             if(opcode.find("SR")!=-1)
             {
-                //cout<<"here 12";
+                //out<<"SR called"<<endl;
+
                 temp=getR();
                 mem.setmem(temp,ctemp);
                 
@@ -258,7 +222,8 @@ public:
             
             if(opcode.find("CR")!=-1)
             {
-             //   cout<<"here 13";
+                //out<<"CR called"<<endl;
+
                 temp=getR();
                 temp1=mem.getmem(ctemp);
                 if(temp==temp1)
@@ -275,7 +240,8 @@ public:
             
             if(opcode.find("BT")!=-1)
             {
-                //cout<<"here 14";
+                //out<<"BT called"<<endl;
+
                 if(C=='T')
                 setIC(ctemp);
                 
@@ -283,12 +249,14 @@ public:
             
             if(opcode.find("H")!=-1)
             {
-               // cout<<"here 15";
+                //out<<"H called"<<endl;
+
                 SI=3;
+                 mos();
             }
             
             
-            mos();
+           // mos();   As mos is called everytime it calls function from mos with previously set interrupt;
             
             
         }
@@ -303,28 +271,25 @@ public:
     {
         switch(SI)
         {
-            case 1:
-               // out<<"GD calling"<<endl;
-                //cout<<line<<endl;
-                line.clear();
-                getline(in,line);
-               // cout<<line<<(ctemp/10)*10<<endl;
-                mem.StoreCardInMemory(line,(ctemp/10)*10);
-                //mem.setmem();
+            case 1: line.clear();
+                    getline(in,line);
+                    mem.StoreCardInMemory(line,(ctemp/10)*10);
+                    break;
+               
+     
+            
+            case 2://out<<"PDCALL"<<endl;
+                    mem.putdatafrommemory((ctemp/10)*10);
+                   break;
                 
-            
-            
-            
-        case 2:
-                //out<<"PD calling"<<endl;
-                //cout<<line<<(ctemp/10)*10<<endl;
-                mem.putdatafrommemory((ctemp/10)*10);
-                    
                 
             
         case 3: terminated='T';
-                out<<"-------------------------------------------------------------"<<endl<<endl;// print dash lines
-        
+                out<<"-------------------------------------------------------------"<<endl<<endl;
+                //terminated='F';
+                //C='F';
+                //SI=0;
+                break;
         }
         
         
@@ -343,42 +308,41 @@ int main()
     {
         line.clear();
         getline(in,line);
-        //cout<<line<<endl;
         if(line.find("$AMJ")!=-1)
         {
-            // cout<<"I am here 1";
             mem.resetmem();
-            //i=0;
-            //continue;
+            
 
         }
         
         else if(line.find("$DTA")!=-1)
         {
-            //cout<<"I am here 2";
-            //cpuobj.setIC(0);
             cpuobj.StartExecution();
-           // continue;
+            continue;
         }
         
         else if(line.find("END")!=-1)
         {
-             //cout<<"I am here 3";
             continue;
         }
         
         else
         {
-             //cout<<"I am here 4";
             mem.StoreCardInMemory(line,memcnt);
-            //cout<<mem.m[0][0]<<mem.m[0][1]<<mem.m[0][2]<<mem.m[0][3]<<endl;
-
             memcnt=memcnt+10;
+           // memcnt=memcnt+line.length()/4;
         }
-        //out<<endl;
         
     }
     
+   /* for (i=0; i<100; i++)
+    {
+        for(j=0;j<4;j++)
+    {
+        cout<<mem.m[i][j];
+    }
+        cout<<endl;
+    }*/
     in.close();
     out.close();
     return 0;
