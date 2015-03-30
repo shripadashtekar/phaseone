8e72636533466e5a9362effc6984041c6e813432
shripadashtekar <shripadashtekar7@gmail.com>
2015-03-31 01:10:15 +0530

    phase 2 in progress.

--- phasetwo.cpp
+++ phasetwo.cpp
@@ -103,9 +103,9 @@ class cpu
 {
     
 private:
-    char IR[4],IC[2],R[4],C,terminated,run_mos,pos;
+    char IR[4],IC[2],R[4],C,terminate,run_mos,pos;
     int returnedIC,ctemp;
-    string returnedIR,temp,operand,opcode,temp1;
+    string returnedIR,temp,operand,opcode,temp1,compare_string;
     
 public:
    
@@ -198,9 +198,9 @@ public:
     void  StartExecution()
     {
         setIC(0);
-        terminated='F';
+        terminate='F';
 
-        while (terminated!='T')
+        while (terminate!='T')
         {
             run_mos='F';
             returnedIC=translateaddress(getIC());
@@ -221,37 +221,76 @@ public:
 
                 if(opcode.find("GD")!=-1)
                 {
-                 SI=1;
-                 //mos();
+                    if(pos!=-1)
+                    {
+                        TLC++;
+                        SI=1;
+                    }
+                    run_mos='T';
                 
                 }
             
                 if(opcode.find("PD")!=-1)
                 {
-                 SI=2;
-                 //mos();
+                    if(pos!=-1)
+                    {
+                        TLC++;
+                        SI=2;
+                    }
+                    run_mos='T';
                 
                 }
             
                 if(opcode.find("LR")!=-1)
                 {
-                temp=mem.getmem(ctemp);
-                setR(temp);
+                    if(pos==-1)
+                    {
+                        run_mos='T';
+                    }
+                    else
+                    {
+                        //temp=mem.getmem(ctemp);
+                        setR(pos);
+                    }
                 }
             
                 
                 if(opcode.find("SR")!=-1)
                 {
-                temp=getR();
-                mem.setmem(temp,ctemp);
+                    temp=getR();
+                    
+                    if(pos==-1)
+                    {
+                        run_mos='T';
+                    }
+                    else
+                    {
+                        mem.setmem(temp,pos);
+                        TLC++;
+                    }
                 }
             
                 
                 if(opcode.find("CR")!=-1)
                 {
                  temp=getR();
-                 temp1=mem.getmem(ctemp);
+                 //temp1=mem.getmem(ctemp);
                     
+                    if(pos==-1)
+                    {
+                        run_mos='T';
+                    }
+                    
+                    else
+                    {
+                        TLC++;
+                        compare_string=mem.getmem(pos);
+                        if(temp.compare(compare_string)==0)
+                            C='T';
+                        else
+                            C='F';
+                    }
+                    /*
                     if(temp==temp1)
                     {
                     C='T';
@@ -260,24 +299,44 @@ public:
                     else
                     {
                     C='F';
-                    }
+                    }*/
+                    
+                    
                 
                 }
             
                 if(opcode.find("BT")!=-1)
                 {
-                if(C=='T')
-                setIC(ctemp);
+                    if(C=='T')
+                    setIC(s_to_i(operand));
+                
+                    TLC++;
                 }
             
                 if(opcode.find("Hr")!=-1)
                 {
-                 SI=3;
-                 //mos();
+                    TLC++;
+                    returnedIR='H';
+                    SI=3;
+                    run_mos='T'
+                 
                 }
+            }
+            else
+            {
+                run_mos='T';
+            }
             
+            if(TLC>TTL)
+            {
+                TI=2;
+                run_mos='T';
+                terminate='T';
+                terminate_code=3;
+            }
             
-           mos();//   As mos is called everytime it calls function from mos with previously set interrupt;
+            if(run_mos=='T')
+            mos();//   As mos is called everytime it calls function from mos with previously set interrupt;
             
             
         }
@@ -290,7 +349,7 @@ public:
     
    void mos ()
     {
-        switch(SI)
+        /*switch(SI)
         {
             case 1: line.clear();
                     getline(in,line);
@@ -305,6 +364,162 @@ public:
                    break;
         }
         SI=0;
+         */
+        if(TI==0 && SI==1)
+        {
+            //get the instrution of the file in terms of 4 words
+            //store it one by one into memory
+            //at start store the program card from memory location with unit place 0
+            
+            string s;
+            pos=(pos/10)*10;
+            getline(in,s);
+            if(s.find("$END")!=-1)
+            {
+                terminate='T';
+                terminate_code=1;
+            }
+            else
+            {
+                if(!s.empty() && s[s.size()-1]=='\r')
+                    s.erase(s.size()-1);
+                int len=s.length(),start=0,i;
+                string s1;
+                for(i=pos;start<len;i++)
+                {
+                    if((len-start)<4)
+                        s1=s.substr(start,(len-start));
+                    else
+                        s1=s.substr(start,4);
+                    start+=4;
+                    mem.setmem(s1,i);
+                }
+            }
+            SI=0;
+        }
+        else if(TI==2 && SI==1)
+        {
+            terminate='T';
+            terminate_code=3;
+        }
+        else if((TI==0 || TI==2) && SI==2)
+        {
+            LLC++;
+            if(LLC>TLL)
+            {
+                terminate='T';
+                terminate_code=2;
+            }
+            //put the data from memory into the file specified
+            else
+            {
+                int pos=address_tranlation(s_to_i(operand)),flag=0;
+                pos=(pos/10)*10;
+                string ans="",temp="";
+                for(int i=pos;i<pos+10;i++)
+                {
+                    temp=mem.getmem(i);
+                    for(int j=0;j<4;j++)
+                    {
+                        if(temp[j]=='\0' || temp[j]=='$')
+                        {
+                            break;
+                            flag=1;
+                        }
+                        ans+=temp[j];
+                    }
+                    if(flag)
+                        break;
+                }
+                out<<ans<<endl;
+                if(TI==2)
+                {
+                    terminate='T';
+                    terminate_code=3;
+                }
+            }
+            SI=0;
+        }
+        else if((TI==0 || TI==2) && SI==3)
+        {
+            //make terminate true to end the program
+            //as we encountered the halt instruction
+            
+            terminate='T';
+            terminate_code=0;
+        }
+        else if(TI==0 && PI==1)
+        {
+            terminate_code=4;
+            terminate='T';
+        }
+        else if(TI==0 && PI==2)
+        {
+            terminate_code=5;
+            terminate=true;
+        }
+        
+        else if(TI==0 && PI==3)
+        {
+            if(!(opreator.compare("GD")) || !(opreator.compare("SR")))
+            {
+                int page_no=pcbobj.allocatepage();
+                pcbobj.initializepagetable((s_to_i(operand))/10,page_no);
+                setIC(getIC()-1);
+            }
+            else
+            {
+                terminate_code=6;
+                terminate='T';
+            }
+        }
+        
+        else if(TI==2 && PI==1)
+        {
+            terminate_code=7;
+            terminate='T';
+        }
+        
+        else if(TI==2 && PI==2)
+        {
+            terminate_code=8;
+            terminate='T';
+        }
+        
+        else if(TI==2 && PI==3)
+        {
+            terminate_code=3;
+            terminate='T';
+        }
+        if(terminate=='T')
+        {
+            out<<p_id<<" ";
+            switch(terminate_code)
+            {
+                case 0:out<<"NO ERROR\n";
+                    break;
+                case 1:out<<"OUT OF DATA\n";
+                    break;
+                case 2:out<<"LINE LIMIT EXCEEDED\n";
+                    break;
+                case 3:out<<"TIME LIMIT EXCEEDED\n";
+                    break;
+                case 4:out<<"OPERATION CODE ERROR\n";
+                    break;
+                case 5:out<<"OPERAND ERROR\n";
+                    break;
+                case 6:out<<"INVALID PAGE FAULT\n";
+                    break;
+                case 7:out<<"TIME LIMIT EXCEEDED with OPERATION CODE ERROR\n";
+                    break;
+                case 8:out<<"TIME LIMIT EXCEEDED with OPERAND ERROR\n";
+                    break;
+            }
+            out<<getIC()<<" "<<returnedIR<<" "<<TLC<<" "<<LLC<<endl;
+            out<<"-----------------------------------------------------------------------"<<endl<<endl;
+        }
+    }
+
         
         
     }
@@ -398,8 +613,8 @@ int main()
         
         else
         {
-            mem.StoreCardInMemory(line,memcnt);
-            memcnt=memcnt+10;
+            mem.StoreCardInMemory(line,memcnt++);
+            //memcnt=memcnt+10;
         }
         
     }
